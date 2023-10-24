b1a4f9284b7c40623e741acd76173f528bc271fa
Ronan Henry <ronan.henry@net-helium.fr>
2023-10-24 11:25:07 +0200

    initial commit

---
+++ init.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Redmine::Plugin.register :pops_onlyoffice do
+  name 'Pops Onlyoffice plugin'
+  author 'Net Helium'
+  description 'This plugin overrides the ONLYOFFICE plugin for Pops specific changes'
+  version '0.0.1'
+  url 'https://github.com/LETG/redmine-pops-onlyoffice'
+  author_url 'https://net-helium.fr'
+end
