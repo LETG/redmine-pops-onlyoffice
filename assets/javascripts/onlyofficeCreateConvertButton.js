3f62741aeb3d4e07823537c33c5049f79aee3671
Ronan Henry <ronan.henry@net-helium.fr>
2024-01-24 07:34:34 +0100

    Modification des vues spécifiques OnlyOffice pour le projet Pops

---
+++ assets/javascripts/onlyofficeCreateConvertButton.js
@@ -0,0 +1,30 @@
+(function () {
+    if (!window.ONLYOFFICE) ONLYOFFICE = {};
+    if (!window.ONLYOFFICE.Buttons) ONLYOFFICE.Buttons = {};
+
+    function createConvertButton(is_edit, text, image) {
+        var downloadButton = document.getElementsByClassName("contextual")[0].getElementsByClassName("icon icon-download")[0];
+        var button = document.createElement("a");
+
+        button.innerText = text;
+        button.className = "onlyoffice-editor-button icon";
+        button.style.backgroundImage = image;
+        button.style.backgroundSize = "16px";
+        button.style.cursor = "pointer";
+
+        if (is_edit) {
+            let attachmentHref = window.location.href;
+            button.onclick = function () {
+                window.location.href = window.location.origin + "/pops/onlyoffice/editor" + attachmentHref.substring(attachmentHref.lastIndexOf("/"))
+            }
+        } else {
+            button.href = "#";
+            button.onclick = function () {
+                ONLYOFFICE.Convert.displayFormDownloadAs();
+            }
+        }
+        downloadButton.parentElement.insertBefore(button, downloadButton);
+    }
+
+    ONLYOFFICE.Buttons.createConvertButton = createConvertButton;
+})();
